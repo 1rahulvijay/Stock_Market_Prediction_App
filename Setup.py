1c44321beb7d0c8b8e07720a712afffde881429a
Rahul Vijayvargiya <33434588+1rahulvijay@users.noreply.github.com>
2024-07-17 19:30:17 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,15 +1,24 @@
 # setup.py
 
+# setup.py
+
 from setuptools import setup, find_packages
 
 setup(
     name='mypackage',
     version='0.1',
-    packages=find_packages(),
-    description='A simple example package with classes',
+    packages=find_packages(),  # Automatically finds the package(s)
+    description='A simple ETL package',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/mypackage',
+    install_requires=[
+        'oracledb',
+        'pyarrow',
+        'pandas',
+        'colorama',
+        'yaml',
+        'sqlalchemy',
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
